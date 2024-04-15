8add3a2d1875aecfb7b98f7d9e553dcc2beb26b9
LukasGruber <gruberlukas1197@gmail.com>
2024-04-15 23:02:10 +0200

    added eventdetails and home page

---
+++ src/EventPlatform/EventService.Server/Core/Entities/Event.cs
@@ -0,0 +1,19 @@
+﻿namespace EventService.Server.Core.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        public string EventName { get; set; }
+
+        public string EventType { get; set; } //Konzert, Fußballspiel, etc.
+
+        public string EventLocation { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public int AvailableTickets { get; set; }
+
+        public decimal TicketPrice { get; set; }
+    }
+}
